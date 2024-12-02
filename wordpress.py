b291a02c334a4060b6ac54db57a0dd4b084f0af9
Greg Edelston <gredelston@gmail.com>
2024-12-01 21:29:30 -0700

    set event categories upon upload

--- wordpress.py
+++ wordpress.py
@@ -1,9 +1,6 @@
-"""Interface with our Wordpress website.
+"""Interface with our Wordpress website."""
 
-For documentation on the Events Calendar API, see:
-https://docs.theeventscalendar.com/rest-endpoints/
-"""
-
+import functools
 import logging
 from typing import Any
 
@@ -17,14 +14,32 @@ import exceptions
 # This is exposed in the Wordpress theme's functions.php.
 WP_META_KEY_MEETUP_EVENT_ID = "meetup_event_id"
 
-def _get_tag_for_meetup_event_id(event_id: int) -> str:
-    """Return a Wordpress tag to match the Meetup Event ID."""
-    return f"_meetup_event_id:{event_id}"
-
-def _get_wordpress_events_api_url(wordpress_url: str, endpoint: str) -> str:
+def _get_wordpress_api_url(wordpress_url: str, endpoint: str) -> str:
     """Return a Wordpress URL we can send HTTP requeasts for events to."""
     endpoint = endpoint.lstrip("/")
     return wordpress_url.rstrip("/") + f"/wp-json/wp/v2/{endpoint}"
+
+
+@functools.cache
+def _get_category_ids(cfg: config.Config) -> dict[events.EventCategory, int]:
+    """Determine the Wordpress category ID for each EventCategory.
+
+    This function is cached so that we don't waste time GET-ing the category IDs
+    every time we want to publish an event. For now that's fine because the
+    script doesn't run for a long time, so the categories are unlikely to change
+    while the script runs. But if someone were to modify this script to be
+    long-running, then we should invalidate the cache after some timeout.
+    """
+    response = requests.get(
+        _get_wordpress_api_url(cfg.wordpress_url, "tribe_events_cat"),
+        timeout=30,
+    )
+    ids_by_category: dict[events.EventCategory, int] = {}
+    for category_json in response.json():
+        name = category_json["name"]
+        category = events.EventCategory[name.upper()]
+        ids_by_category[category] = category_json["id"]
+    return ids_by_category
 
 
 def get_wordpress_events(
@@ -33,7 +48,7 @@ def get_wordpress_events(
 ) -> dict[str, Any]:
     """Return a list of events already on Wordpress."""
     response = requests.get(
-        _get_wordpress_events_api_url(cfg.wordpress_url, "events"),
+        _get_wordpress_api_url(cfg.wordpress_url, "events"),
         params=params,
         auth=cfg.wordpress_credentials,
         timeout=30,
@@ -63,6 +78,7 @@ def upload_event(
     dryrun: bool = False,
 ) -> None:
     """Upload an event to Wordpress."""
+    # TODO: why am I unable to view my published events on Wordpress?
     if not event.get_categories():
         raise exceptions.UndeterminedEventCategoryException(event.title)
     post_data = {
@@ -73,10 +89,11 @@ def upload_event(
         "start_date": event.start_time.strftime("%Y-%m-%d %H:%M:%S"),
         "end_date": event.end_time.strftime("%Y-%m-%d %H:%M:%S"),
         "website": event.url,
-        "categories": [c.value for c in event.get_categories()],
+        "tribe_events_cat": [
+            _get_category_ids(cfg)[c] for c in event.get_categories()],
         "meta": {WP_META_KEY_MEETUP_EVENT_ID: str(event.meetup_id)},
     }
-    api_url = _get_wordpress_events_api_url(cfg.wordpress_url, "/events")
+    api_url = _get_wordpress_api_url(cfg.wordpress_url, "/events")
     logging.info("Trying to create event: %s", event.title)
     logging.debug(
         "Sending POST request to %s with data: %s", api_url, post_data)
